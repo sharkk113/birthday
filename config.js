af84e8d4ec13997fd85a35808ef1b04bd47eb381
sharkk113 <0868906092linh@gmail.com>
2024-11-03 21:27:02 +0700

    birthday

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng em';
+const contentCard = 'Chúc em sinh nhật zui ze. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'thuyanh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
